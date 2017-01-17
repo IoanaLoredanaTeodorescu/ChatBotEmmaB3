5d6585c96a725eecffd2ec01c8e6ac9330f65e32
RoxanaPantea <pnt.roxana15@gmail.com>
2017-01-17 17:05:23 +0200

    integrated

--- Modulul 1/Backend/prog.py
+++ Modulul 1/Backend/prog.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-import MySQLdb, aiml,os,json
+import MySQLdb, aiml,os
 from memory_check import Memory
 import threading
 from mood import Mood
@@ -9,6 +9,10 @@ from personality import *
 from randomAnswer import RandomAnswer
 import random
 import isEnglishOrJibberish
+from time import *
+from split_sentences import *
+import timeit #linia 1
+
 app = Flask(__name__)
 
 db = MySQLdb.connect(host="ppdatabase.ccvycmsqlp8u.eu-central-1.rds.amazonaws.com",    # your host, usually localhost
@@ -22,11 +26,12 @@ cur = db.cursor()
 
 # for row in cur.fetchall():
 #     print row[0]
-cur.execute("SELECT * FROM persons")
+cur.execute("SELECT * FROM Users")
 
 table= cur.fetchall()
 cur.execute("SELECT * FROM questions")
 questions = cur.fetchall()
+
 db.close()
 
 rnd = RandomAnswer()
@@ -42,6 +47,8 @@ botMood = Mood()
 
 attr=getPeopleAttributes()
 iKnowOp=2
+
+start = 0
 pairQA_dict={}
 res=""
 # Press CTRL-C to break this loop
@@ -49,10 +56,13 @@ sendNew_res=0
 #load json
 json_data=open("QA.json").read()
 all_QA=json.loads(json_data)
+# Press CTRL-C to break this loop
 @app.route('/<question>')
 def main(question):
+    stop = timeit.default_timer()
     global attr
     global iKnowOp
+    global start
     global pairQA_dict
     global res
     global sendNew_res
@@ -60,39 +70,38 @@ def main(question):
     #users=table
     bootMemory= Memory()
     if not sendNew_res and res.endswith('?'):
-        if len(attr)==1:
-            pers=attr[0]
+        if len(attr) == 1:
+            pers = attr[0]
             print res
             print all_QA[str(pers[0])].keys()
             if res in all_QA[str(pers[0])].keys():
-                aux_d=all_QA[str(pers[0])]
-                if aux_d[res].lower()!=question.lower():
+                aux_d = all_QA[str(pers[0])]
+                if aux_d[res].lower() != question.lower():
                     # print aux_d[res]
-                    new_res= "But last time you said other thing. I quote: " +str(aux_d[res])
-                    aux_d[res]=question
-                    sendNew_res=1
+                    new_res = "But last time you said other thing. I quote: " + str(aux_d[res])
+                    aux_d[res] = question
+                    sendNew_res = 1
                     return new_res
-        pairQA_dict[res]=question
-    sendNew_res=0
+        pairQA_dict[res] = question
+    sendNew_res = 0
     res=kernel.respond(question,sessionId)
-    if res=='CLOSING SESSION':
+    if res == 'CLOSING SESSION':
         print pairQA_dict
-        if len(attr)==1:
+        if len(attr) == 1:
             pers = attr[0]
             # memorate_QA(pers[0],pairQA_dict)
             if pairQA_dict is not None:
                 if str(pers[0]) in all_QA:
                     all_QA[str(pers[0])].update(pairQA_dict)
                 else:
-                    all_QA[pers[0]]=pairQA_dict
+                    all_QA[pers[0]] = pairQA_dict
                 twitterDataFile = open("QA.json", "w")
                 # magic happens here to make it pretty-printed
                 twitterDataFile.write(json.dumps(all_QA, indent=4, sort_keys=True))
                 twitterDataFile.close()
-        elif len(attr)==0:
-            memoratePerson(kernel,sessionId)
+        elif len(attr) == 0:
+            memoratePerson(kernel, sessionId)
         exit(1)
-
     responses_incase_nonenglish = open('jibberish_responses.json', 'r').read()
     jibberish_array = json.loads(responses_incase_nonenglish)
     if not (isEnglishOrJibberish.is_english_sentence(question)):
@@ -108,17 +117,23 @@ def main(question):
     if len(attr)>1:
         attr=verifyExistence(kernel,sessionId,attr)
     if len(attr)==1:
-        iKnowOp=1
-        pers=attr[0]
-        updateAttributes(kernel,attr[0],sessionId)
+        iKnowOp = 1
+        pers = attr[0]
+        updateAttributes(kernel, attr[0], sessionId)
     if len(attr)==0:
-        iKnowOp=0
+        iKnowOp = 0
         decideToMemorate(kernel,sessionId)
+
     if res=='':
         res = rnd.answer(questions)
+    time = stop - start
+    if time > 20:
+        time_obj = Time_answer()
+        res = time_obj.time_answer(time)
 
     bootMemory.addResponse(res)
-
+    print stop - start
+    start = timeit.default_timer()
     return res
 
 
